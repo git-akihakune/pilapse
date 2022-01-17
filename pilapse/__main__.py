57d0217a3d1f6ae82907f1178cd7b2596a727f38
git-akihakune <akihakune@protonmail.ch>
2022-01-17 13:55:28 +0000

    Get arguments from CLI

--- pilapse/__main__.py
+++ pilapse/__main__.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
-# dummy main script for testing purpose
 
-from pilapse.__init__ import *
+"""piLapse - capture time lapse image on Raspberry Pi.
 
-print(f"The package's author is {__author__}")
+Usage:
+    pilapse configure
+    pilapse record [-d <duration>] [-f <frequency>] [-F <fps>] [-s <save-dir>] [--preserve] [--verbose]
+    pilapse (-h | --help)
+    pilapse --version
+
+Options:
+-h --help                   Show this screen.
+-d --duration DURATION      Set recording duration (by seconds) [default: 600].
+-f --frequency FREQUENCY    Set time interval between shots (by seconds) [default: 5].
+-F --fps FPS                Set final video fps [default: 24].
+-s --save-dir SAVINGDIR     Set working and saving directory [default: ~/Videos/pilapse].
+-p --preserve               Do not automatically clean up after recording.
+--verbose                   Set to verbose mode.
+--version                   Show version.
+
+
+"""
+from docopt import docopt
+from . import __version__
+
+if __name__  == "__main__":
+    arguments = docopt(__doc__, version=__version__)
+    print(arguments)
