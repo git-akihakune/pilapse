31570405f3e585a5a7040c9fc0d31b27acffd9dc
git-akihakune <akihakune@protonmail.ch>
2022-01-18 20:25:37 +0700

    Add continuous record function

--- pilapse/__main__.py
+++ pilapse/__main__.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
-"""piLapse - capture time lapse image on Raspberry Pi.
+"""piLapse - capture time lapse video on Raspberry Pi.
 
 Usage:
-    pilapse capture [-i <image-name>]
-    pilapse record [-d <duration>] [-f <frequency>] [-l <length>] [-w <width>] [-F <fps>] [-s <save-dir>] [-S <wait-time>] [--no-text] [--preserve] [--verbose]
+    pilapse capture [-i <image-name>] [-s <save-dir>]
+    pilapse record [-d <duration> | -c] [-f <frequency>] [-l <length>] [-w <width>] [-F <fps>] [-s <save-dir>] [-S <wait-time>] [--no-text] [--preserve] [--verbose]
+    pilapse compile [-s <save-dir>]
     pilapse (-h | --help)
     pilapse --version
 
+Actions:
+capture     Take 1 image only.
+record      Record a timelapse video.
+compile     Compile labelled images into a video.
+
 Options:
 -h --help                   Show this screen.
+-c --continuous             Set to continuously run mode.
 -d --duration DURATION      Set recording duration (by seconds) [default: 600].
 -f --frequency FREQUENCY    Set time interval between shots (by seconds) [default: 5].
 -F --fps FPS                Set final video fps [default: 24].
@@ -29,12 +36,15 @@ from docopt import docopt
 from . import __version__
 from . import capture
 
+
 def main():
     arguments = docopt(__doc__, version=__version__)
     camera = capture.camera(workdir=arguments['--save-dir'], length=int(arguments['--length']), width=int(arguments['--width']))
-    if arguments['capture']:
-        camera.capture(arguments['--image-name'])
 
+    if arguments['capture']:
+        camera.capture(imageName=arguments['--image-name'], singleCapture=True)
+    elif arguments['record']:
+        camera.record(duration=int(arguments['--duration']), frequency=int(arguments['--frequency']), continuous=arguments['--continuous'])
 
 if __name__  == "__main__":
     main()
--- pilapse/capture.py
+++ pilapse/capture.py
@@ -2,49 +2,58 @@
 
 class camera:
     def __init__(self, workdir = '~/Videos/pilapse', length: int = 3280, width: int = 2464):
+        from os import chdir, getcwd
         from picamera import PiCamera
         from time import sleep
+
+        self.camera = PiCamera()
+        self.camera.resolution = (length, width)
 
         # process relative paths
         if workdir[0] == '~':
             from pathlib import Path
             homeDir = str(Path.home())
             workdir = workdir.replace('~', homeDir, 1)
-
-        self.camera = PiCamera()
-        self.camera.resolution = (length, width)
+        
+        self._setUpWorkingDir(workdir)
+        chdir(workdir)
 
         # camera warm-up time
         sleep(2)
-
-        self.workdir = workdir
-        self._setWorkingDir()
+        print(f"Successfully set up camera. Working at {getcwd()}")
 
 
-    def _setWorkingDir(self):
+
+    def _setUpWorkingDir(self, workdir: str):
         import os, shutil
 
-        print(f"Working dir at {self.workdir}")
-
-        if not os.path.exists(self.workdir):
+        if not os.path.exists(workdir):
             print("Specified save path not found. Creating one.")
-            os.makedirs(self.workdir, exist_ok=True)
-        elif not os.listdir(self.workdir) == []:
+            os.makedirs(workdir, exist_ok=True)
+        elif not os.listdir(workdir) == []:
             # prompt the user to ask if the specified directory
             # should be deleted
             print("The specified path seems to already contains files. Continue will overwrite all data in that file.")
             if (input("Continue? [y/n] ") == "y"):
-                shutil.rmtree(self.workdir)
-                os.makedirs(self.workdir, exist_ok=True)
+                shutil.rmtree(workdir)
+                os.makedirs(workdir, exist_ok=True)
         else:
-            print(f"{self.workdir} seems already initiated")
+            print(f"{workdir} seems already initiated")
 
 
 
-    def capture(self, imageName: str):
-        from os.path import join
-        self.camera.capture(join(self.workdir, imageName))
+    def capture(self, imageName: str, addTime: bool = False, singleCapture: bool = False):
+        self.camera.capture(imageName)
+        if singleCapture:
+            import os
+            print(f"Image saved at {os.path.join(os.getcwd(), imageName)}")
 
 
-    def record(self, duration: int, frequency: int):
-        pass
+
+    def record(self, duration: int, frequency: int, continuous: bool = False):
+        import os
+        from time import sleep
+        if continuous:
+            for filename in self.camera.capture_continuous('img{counter:03d}.jpg'):
+                print(f"Captured {filename}")
+                sleep(frequency)
